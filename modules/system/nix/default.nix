345c451f60374518f0b4f405fb46f0391bad8655
Asai Neko <sugar@sne.moe>
2026-08-25 01:35:12 +0800

    feat(nix): configure daemon defaults
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/system/nix/default.nix
+++ modules/system/nix/default.nix
@@ -1,8 +1,39 @@
-_: {
-  flake.modules.nixos.nix = {
-    nix.settings.experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
-  };
+{ inputs, ... }:
+{
+  flake.modules.nixos.nix =
+    { pkgs, ... }:
+    {
+      nix = {
+        package = pkgs.nix;
+        channel.enable = false;
+
+        registry.sugar.flake = inputs.self;
+
+        settings = {
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+          extra-substituters = [
+            "https://nix-community.cachix.org"
+          ];
+          trusted-public-keys = [
+            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          ];
+          trusted-users = [ "@wheel" ];
+        };
+
+        nixPath = [ "/etc/nix/path" ];
+
+        gc = {
+          automatic = true;
+          dates = "weekly";
+          options = "--delete-older-than 7d";
+        };
+      };
+
+      environment.etc."nix/path/nixpkgs".source = inputs.nixpkgs;
+
+      nixpkgs.config.allowUnfree = true;
+    };
 }
