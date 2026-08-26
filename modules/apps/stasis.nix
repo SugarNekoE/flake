bc73f8ae3e1b815899afae3e4e0e804e58fa321c
Asai Neko <sugar@sne.moe>
2026-08-27 02:27:58 +0800

    feat(desktop): replace idle handling with Stasis
    Signed-off-by: Asai Neko <sugar@sne.moe>

---
+++ modules/apps/stasis.nix
@@ -0,0 +1,60 @@
+{ inputs, ... }:
+{
+  flake-file.inputs.stasis = {
+    url = "github:saltnpepper97/stasis";
+    inputs = {
+      flake-parts.follows = "flake-parts";
+      nixpkgs.follows = "nixpkgs-unstable";
+    };
+  };
+
+  home =
+    {
+      config,
+      pkgs,
+      ...
+    }:
+    {
+      imports = [ inputs.stasis.homeModules.stasis ];
+
+      services.stasis = {
+        enable = true;
+        target = "graphical-session.target";
+        tray.enable = true;
+        environmentFile = null;
+        extraPathPackages = [
+          config.programs.swaylock.package
+          pkgs.unstable.swayfx
+        ];
+        extraConfig = ''
+          @author "sugar"
+          @description "Sway idle management"
+
+          default:
+            enable_loginctl_integration true
+            enable_dbus_inhibit true
+            prepare_sleep_command "swaylock"
+            monitor_media true
+            ignore_remote_media true
+            notify_on_unpause true
+
+            ac:
+              lock_and_dpms:
+                timeout 1800
+                command "swaylock; swaymsg 'output * power off'"
+                resume_command "swaymsg 'output * power on'"
+              end
+            end
+
+            battery:
+              lock_and_dpms:
+                timeout 1800
+                command "swaylock; swaymsg 'output * power off'"
+                resume_command "swaymsg 'output * power on'"
+              end
+            end
+          end
+        '';
+      };
+    };
+}
