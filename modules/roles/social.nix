267300a5503cb55f23aef71b3fb62617082fd98c
Asai Neko <sugar@sne.moe>
2026-08-27 02:48:06 +0800

    fix(social): install TeamSpeak from Flatpak
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/roles/social.nix
+++ modules/roles/social.nix
@@ -1,11 +1,15 @@
-_: {
+{ inputs, ... }: {
+  imports = with inputs.self.aspects; [
+    (flatpak.withPackages [
+      "com.teamspeak.TeamSpeak3"
+    ])
+  ];
   home = { pkgs, ... }: {
     home.packages = with pkgs; [
       wechat
       qq
       discord
       telegram-desktop
-      teamspeak3
       fractal
     ];
   };
