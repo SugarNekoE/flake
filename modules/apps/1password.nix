47797996af9f9149053cb37df8e21cb597cee67f
Asai Neko <sugar@sne.moe>
2026-08-26 19:16:57 +0800

    feat(ssh): integrate the 1Password agent
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/apps/1password.nix
+++ modules/apps/1password.nix
@@ -13,11 +13,15 @@ _: {
     { config, ... }:
     let
       sshAgentSocket = "${config.home.homeDirectory}/.1password/agent.sock";
+      onePasswordConfig = "${config.home.homeDirectory}/.ssh/1Password/config";
     in
     {
+      home.sessionVariables.SSH_AUTH_SOCK = sshAgentSocket;
+
       programs.ssh = {
         enable = true;
         enableDefaultConfig = false;
+        includes = [ onePasswordConfig ];
         settings."*".IdentityAgent = sshAgentSocket;
       };
     };
