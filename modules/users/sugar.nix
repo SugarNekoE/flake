83edbc30521fc22d32ecc200cbb7f176c905a85e
Asai Neko <sugar@sne.moe>
2026-08-26 21:53:55 +0800

    fix(home-manager): overwrite existing backups
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/users/sugar.nix
+++ modules/users/sugar.nix
@@ -31,6 +31,8 @@
       };
 
       home-manager = {
+        backupFileExtension = "backup";
+        overwriteBackup = true;
         useGlobalPkgs = true;
         useUserPackages = true;
         users.${user.username} = { };
