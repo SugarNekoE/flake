ce6874180cdf6fb7366304494e1744d4da17ae49
Asai Neko <sugar@sne.moe>
2026-08-27 21:26:03 +0800

    refactor(office): migrate apps to Flatpak
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/roles/office.nix
+++ modules/roles/office.nix
@@ -3,6 +3,8 @@
   imports = with inputs.self.aspects; [
     (flatpak.withPackages [
       "cn.wps.wps_365"
+      "org.libreoffice.LibreOffice"
+      "org.mozilla.thunderbird_esr"
     ])
   ];
   home =
@@ -14,8 +16,6 @@
         xmind
         gimp
         krita
-        libreoffice
-        thunderbird
       ];
     };
 }
