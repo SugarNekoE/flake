eeec16741138e3b7a60f5205783d5a486b8f26bb
Asai Neko <sugar@sne.moe>
2026-08-27 21:26:03 +0800

    refactor(work): migrate Slack to Flatpak
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/roles/work.nix
+++ modules/roles/work.nix
@@ -1,11 +1,16 @@
-_: {
+{ inputs, ... }:
+{
+  imports = with inputs.self.aspects; [
+    (flatpak.withPackages [
+      "com.slack.Slack"
+    ])
+  ];
   home =
     { pkgs, ... }:
     {
       home.packages = with pkgs; [
         figma-linux
         wemeet
-        slack
         feishu
       ];
     };
