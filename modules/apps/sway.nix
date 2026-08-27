344808bd7d8bf7e867237d737bbf055f0e8db956
Asai Neko <sugar@sne.moe>
2026-08-27 12:04:52 +0800

    feat(swaylock): restore visual effects
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/apps/sway.nix
+++ modules/apps/sway.nix
@@ -54,9 +54,14 @@ _: {
       stylix.targets.mako.enable = true;
       programs.swaylock = {
         enable = true;
-        package = pkgs.swaylock;
+        package = pkgs.swaylock-effects;
         settings = {
+          clock = true;
           daemonize = true;
+          datestr = "%A, %B %e";
+          effect-blur = "12x4";
+          effect-vignette = "0.2:0.5";
+          fade-in = 0.25;
           font = nixosConfig.stylix.fonts.sansSerif.name;
           font-size = 28;
           ignore-empty-password = true;
@@ -65,6 +70,7 @@ _: {
           indicator-radius = 110;
           indicator-thickness = 8;
           show-failed-attempts = true;
+          timestr = "%H:%M";
         };
       };
       wayland.windowManager.sway = {
