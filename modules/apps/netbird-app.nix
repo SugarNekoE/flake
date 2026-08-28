43964f375d9598ac9c8b428373f8a56a9dd22211
Asai Neko <sugar@sne.moe>
2026-08-28 19:21:38 +0800

    fix(netbird): correct kde wayland scaling
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/apps/netbird-app.nix
+++ modules/apps/netbird-app.nix
@@ -34,11 +34,31 @@ _: {
     };
 
   home =
-    { pkgs, ... }:
+    { lib, pkgs, ... }:
     let
       uiPackage = pkgs.unstable.netbird-ui;
+      launcher = pkgs.writeShellScript "netbird-ui-launcher" ''
+        if [[ "''${XDG_CURRENT_DESKTOP:-}" == *KDE* ]]; then
+          export GDK_BACKEND=wayland
+          export GDK_SCALE=1
+          export GDK_DPI_SCALE=1
+        fi
+        exec ${lib.getExe uiPackage} --daemon-addr=unix:///run/netbird/sock
+      '';
     in
     {
       home.packages = [ uiPackage ];
+
+      xdg.desktopEntries.netbird = {
+        name = "NetBird";
+        genericName = "Mesh VPN";
+        comment = "Connect to NetBird";
+        exec = toString launcher;
+        icon = "${uiPackage}/share/icons/hicolor/256x256/apps/netbird.png";
+        terminal = false;
+        categories = [ "Network" ];
+        startupNotify = false;
+        settings.StartupWMClass = "org.wails.netbird";
+      };
     };
 }
