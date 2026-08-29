1ce763e6da5772e62220f4e2c79d970beebdc365
Asai Neko <sugar@sne.moe>
2026-08-29 15:04:08 +0800

    feat(agents): add pi workflow extensions
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/apps/agents.nix
+++ modules/apps/agents.nix
@@ -15,6 +15,9 @@ let
     "npm:pi-subagents@0.58.0" = "sha256-RWSRVZ8piZhwBJFstt2d7CLCdMBvMrY8d7a/UhcJLyw=";
     "npm:pi-btw@0.4.1" = "sha256-PJKMskXImX2dsyDdjP56DPRRvrPIUpBFl04APGdEE5c=";
     "npm:pi-better-openai@0.1.22" = "sha256-cCrd34XWA5PxmwIuyH7043ruDTYv3BdDGjHpzuQSs2Q=";
+    "npm:pi-studio@0.9.52" = "sha256-W7JBgzpLsFKmP7yZ78DvFvYTE4N8RRZniIdw0vG1bQU=";
+    "npm:pi-auto-reviewer@1.1.0" = "sha256-538TiFxvcqfvNLNJWNm+NurAnqKjdlUpTbNJGwOBjUY=";
+    "npm:pi-sandbox@0.6.5" = "sha256-Ysc58WDgKlX6dGnhJw0gQU7vT8sZ3ecuF5ng60tTxwo=";
     "npm:@ff-labs/pi-fff@0.10.5" = "sha256-KfilZVZohnisnbQ8XO7+50TQzSaIrw6DpLxA5XRIi+w=";
     "npm:@narumitw/pi-usage@0.54.0" = "sha256-7wFMNCnVi6ynJyjcNxoqfTAK+j5xD/PPhSdCD5Fns8Q=";
     "npm:@krfantasy/pi-monokai-pro@0.1.0" = "sha256-7ti+iHnkpKmqQ0rbJHcyU9YC82Cni3kvKmYL0kfCEf4=";
@@ -114,12 +117,17 @@ in
     in
     {
       home = {
-        packages = with pkgs.llm-agents; [
+        packages = [
+          pkgs.bubblewrap
+          pkgs.ripgrep
+          pkgs.socat
+        ]
+        ++ (with pkgs.llm-agents; [
           pi
           codex
           chatgpt
           claude-code
-        ];
+        ]);
         file.".pi/agent/settings.json".source = settingsFile;
       };
     };
