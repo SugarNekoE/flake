7a532aa0eb1736f02d46217aa06e2874d7b7c25b
Asai Neko <sugar@sne.moe>
2026-08-30 17:05:20 +0800

    feat(role): add kernel-dev role
    Signed-off-by: Asai Neko <sugar@sne.moe>

---
+++ modules/roles/kernel-dev.nix
@@ -0,0 +1,26 @@
+{ inputs, ... }:
+{
+  flake-file.inputs.kernel-dev = {
+    url = "path:/mnt/data/Projects/kernel-dev";
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixos =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      cfg = config.kernelDev;
+      kernelModule = inputs.kernel-dev.nixosModules.default { inherit pkgs; };
+    in
+    {
+      options.kernelDev.enable = lib.mkEnableOption "the locally developed Linux kernel";
+
+      config = lib.mkIf cfg.enable {
+        boot.kernelPackages = lib.mkForce kernelModule.boot.kernelPackages;
+      };
+    };
+}
