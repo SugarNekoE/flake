3eed7d9e828a7f836b6576bfec3ec27ad221006a
Asai Neko <sugar@sne.moe>
2026-08-30 18:14:57 +0800

    refactor(kernel-dev): use local git link for kernel inputs
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/system/nix/kernel-dev.nix
+++ modules/system/nix/kernel-dev.nix
@@ -1,7 +1,7 @@
 { inputs, ... }:
 {
   flake-file.inputs.kernel-dev = {
-    url = "path:/mnt/data/Projects/kernel-dev";
+    url = "git+file:///mnt/data/Projects/kernel-dev";
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
