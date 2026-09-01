34da6f11d7a2f39207322fad39715367365edc0e
Asai Neko <sugar@sne.moe>
2026-09-01 13:34:53 +0800

    feat(unix-tools): add conventional commits for lazygit
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/apps/unix-tools.nix
+++ modules/apps/unix-tools.nix
@@ -86,15 +86,6 @@
         options = [ "--cmd cd" ];
       };
 
-      programs.lazygit = {
-        enable = true;
-        enableFishIntegration = true;
-        settings = {
-          git.commit.signOff = true;
-          services."forge.asnk.io" = "gitea:forge.asnk.io";
-        };
-      };
-
       programs.zellij = {
         enable = true;
         settings = {
@@ -106,6 +97,109 @@
       programs.nix-index = {
         enable = true;
         enableFishIntegration = true;
+      };
+
+      programs.lazygit = {
+        enable = true;
+        enableFishIntegration = true;
+        settings = {
+          git.commit.signOff = true;
+          services."forge.asnk.io" = "gitea:forge.asnk.io";
+          customCommands = [
+            {
+              key = "C";
+              context = "files";
+              description = "Create a Conventional Commit";
+              loadingText = "Creating commit";
+              prompts = [
+                {
+                  type = "menu";
+                  title = "Commit type";
+                  key = "Type";
+                  options = [
+                    {
+                      name = "build";
+                      description = "Build system or dependencies";
+                      value = "build";
+                      key = "b";
+                    }
+                    {
+                      name = "chore";
+                      description = "Maintenance work";
+                      value = "chore";
+                      key = "c";
+                    }
+                    {
+                      name = "ci";
+                      description = "CI configuration";
+                      value = "ci";
+                      key = "i";
+                    }
+                    {
+                      name = "docs";
+                      description = "Documentation";
+                      value = "docs";
+                      key = "d";
+                    }
+                    {
+                      name = "feat";
+                      description = "New feature";
+                      value = "feat";
+                      key = "f";
+                    }
+                    {
+                      name = "fix";
+                      description = "Bug fix";
+                      value = "fix";
+                      key = "x";
+                    }
+                    {
+                      name = "perf";
+                      description = "Performance improvement";
+                      value = "perf";
+                      key = "p";
+                    }
+                    {
+                      name = "refactor";
+                      description = "Code restructuring";
+                      value = "refactor";
+                      key = "r";
+                    }
+                    {
+                      name = "revert";
+                      description = "Revert a previous change";
+                      value = "revert";
+                      key = "v";
+                    }
+                    {
+                      name = "style";
+                      description = "Formatting only";
+                      value = "style";
+                      key = "s";
+                    }
+                    {
+                      name = "test";
+                      description = "Tests";
+                      value = "test";
+                      key = "t";
+                    }
+                  ];
+                }
+                {
+                  type = "input";
+                  title = "Scope";
+                  key = "Scope";
+                }
+                {
+                  type = "input";
+                  title = "Commit message";
+                  key = "Message";
+                }
+              ];
+              command = ''git commit --signoff --message "$(printf '%s(%s): %s' {{.Form.Type | quote}} {{.Form.Scope | quote}} {{.Form.Message | quote}})"'';
+            }
+          ];
+        };
       };
 
       stylix.targets = {
