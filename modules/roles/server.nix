148576589fec194ef337cefd6f2cb4f214391285
Asai Neko <sugar@sne.moe>
2026-09-01 14:51:11 +0800

    feat(roles): add server layout role
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/roles/server.nix
+++ modules/roles/server.nix
@@ -1,4 +1,44 @@
-{ inputs, ... }:
-{
-  imports = with inputs.self.aspects; [ git.nixos ];
+{ inputs, ... }: {
+  imports = with inputs.self.aspects; [
+    openssh
+    podman
+    networkmanager.nixos
+  ];
+
+  nixos = {
+    boot.kernel.sysctl = {
+      "net.ipv4.ip_forward" = "1";
+      "net.ipv6.conf.all.forwarding" = "1";
+      "net.ipv4.tcp_congestion_control" = "bbr";
+      "net.ipv6.tcp_congestion_control" = "bbr";
+    };
+
+    systemd = {
+      enableEmergencyMode = false;
+      settings.Manager = {
+        RuntimeWatchdogSec = "20s";
+        RebootWatchdogSec = "30s";
+        KExecWatchdogSec = "1m";
+      };
+    };
+
+    documentation = {
+      man.enable = false;
+      dev.enable = false;
+      doc.enable = false;
+      nixos.enable = false;
+    };
+
+    fonts.fontconfig.enable = false;
+
+    programs.git.enable = true;
+    programs.vim = {
+      enable = true;
+      defaultEditor = true;
+    };
+
+    users.mutableUsers = false;
+
+    networking.firewall.enable = true;
+  };
 }
