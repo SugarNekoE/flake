02366fb9aeefe757c3dab1b7ec7399045204f9d2
Asai Neko <sugar@sne.moe>
2026-09-01 15:56:34 +0800

    feat(identity): add aster machine ssh-key
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/identity/ssh-keys.nix
+++ modules/identity/ssh-keys.nix
@@ -1,7 +1,11 @@
 _: {
   identity.sshKeys = {
+    # users
+    nvirellia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOOz0CMmkGSXv4H77rmrmvadltAlwAZeVimxGoUAfArs";
+    # desktops
     daisy = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKOSbYIOvtjrNmMdViOACfECla6LB8GKYB/mxtfJNVFh";
     fern = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKGT4gTVaCt6cQdFL+vcI7zS6TIkrVyZkltXD6g3lET3";
-    nvirellia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOOz0CMmkGSXv4H77rmrmvadltAlwAZeVimxGoUAfArs";
+    # servers
+    aster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILXCIMtqjBTbbLmfXhkU0hoKoCLK0qwQ7CtK6rYIDgl9";
   };
 }
