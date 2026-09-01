e361ba07f0907bbd42c03033f2e7d61acbfc2006
Asai Neko <sugar@sne.moe>
2026-09-01 15:57:20 +0800

    feat(sing-box): add sops config path
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/services/sing-box.nix
+++ modules/services/sing-box.nix
@@ -1,18 +1,28 @@
-{
-  nixos =
-    { lib, ... }:
-    let
-      sopsFile = ../secrets/sing-box.json;
-      hasConfig = builtins.pathExists sopsFile;
-    in
+{ inputs, ... }:
+let
+  withProfile =
+    { sopsFile }:
     {
-      sops.secrets.sing_box_config = lib.mkIf hasConfig {
-        format = "json";
-        inherit sopsFile;
-        path = "/etc/sing-box/config.json";
-        key = "";
-        restartUnits = [ "sing-box.service" ];
-      };
-      services.sing-box.enable = hasConfig;
+      _class = "aspects";
+      imports = [ inputs.self.modules.aspects.sing-box ];
+      nixosModule =
+        { lib, ... }:
+        let
+          hasProfile = builtins.pathExists sopsFile;
+        in
+        lib.mkIf hasProfile {
+          sops.secrets.sing_box_config = {
+            format = "json";
+            inherit sopsFile;
+            path = "/etc/sing-box/config.json";
+            key = "";
+            restartUnits = [ "sing-box.service" ];
+          };
+        };
     };
+in
+{
+  aspectHelpers.sing-box = { inherit withProfile; };
+
+  nixos.services.sing-box.enable = true;
 }
