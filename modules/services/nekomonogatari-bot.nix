5c7414034429fb98657bae0e89063e5ee3e04c3f
Asai Neko <sugar@sne.moe>
2026-09-02 18:31:20 +0800

    feat(nekomonogatari-bot): add cachix
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/services/nekomonogatari-bot.nix
+++ modules/services/nekomonogatari-bot.nix
@@ -5,6 +5,13 @@
   nixos = {
     imports = [ inputs.nekomonogatari-bot.nixosModules.default ];
 
+    nix.settings = {
+      substituters = [ "https://nekomonogatari-bot.cachix.org" ];
+      trusted-public-keys = [
+        "nekomonogatari-bot.cachix.org-1:Opwduz2myeSTFELnIcJvvmJ97yofQJSdl5SBqrdUby0="
+      ];
+    };
+
     sops.secrets.nekomonogatari-bot = {
       sopsFile = ../secrets/nekomonogatari-bot.yaml;
       key = "";
