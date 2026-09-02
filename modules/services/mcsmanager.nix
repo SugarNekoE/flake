7ee8f9d6027e89dbdb7f2139ea0c823e3415f117
Asai Neko <sugar@sne.moe>
2026-09-03 01:15:23 +0800

    feat(mcsmanager): add to nixosModule
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/services/mcsmanager.nix
+++ modules/services/mcsmanager.nix
@@ -1,60 +1,62 @@
 let
   installPath = "/opt/mcsmanager";
 in
-{ config, ... }: {
-  oci-containers = {
-    backend = "podman";
-    containers = {
-      mcsmanager-web = {
-        image = "docker.io/githubyumao/mcsmanager-web:latest";
+{
+  nixos = { config, ... }: {
+    oci-containers = {
+      backend = "podman";
+      containers = {
+        mcsmanager-web = {
+          image = "docker.io/githubyumao/mcsmanager-web:latest";
 
-        ports = [
-          "23333:23333"
-        ];
+          ports = [
+            "23333:23333"
+          ];
 
-        volumes = [
-          "/etc/localtime:/etc/localtime:ro"
-          "${installPath}/web/data:/opt/mcsmanager/web/data"
-          "${installPath}/web/logs:/opt/mcsmanager/web/logs"
-        ];
-      };
-
-      mcsmanager-daemon = {
-        image = "docker.io/githubyumao/mcsmanager-daemon:latest";
-
-        ports = [
-          "24444:24444"
-        ];
-
-        environment = {
-          MCSM_DOCKER_WORKSPACE_PATH = "/opt/mcsmanager/instance";
+          volumes = [
+            "/etc/localtime:/etc/localtime:ro"
+            "${installPath}/web/data:/opt/mcsmanager/web/data"
+            "${installPath}/web/logs:/opt/mcsmanager/web/logs"
+          ];
         };
 
-        volumes = [
-          "/etc/timezone:/etc/timezone:ro"
-          "/etc/localtime:/etc/localtime:ro"
+        mcsmanager-daemon = {
+          image = "docker.io/githubyumao/mcsmanager-daemon:latest";
 
-          "${installPath}/daemon/data:/opt/mcsmanager/daemon/data"
-          "${installPath}/daemon/logs:/opt/mcsmanager/daemon/logs"
-          "${installPath}/instance:/opt/mcsmanager/instance"
+          ports = [
+            "24444:24444"
+          ];
 
-          "/run/docker.sock:/var/run/docker.sock"
-        ];
+          environment = {
+            MCSM_DOCKER_WORKSPACE_PATH = "/opt/mcsmanager/instance";
+          };
+
+          volumes = [
+            "/etc/timezone:/etc/timezone:ro"
+            "/etc/localtime:/etc/localtime:ro"
+
+            "${installPath}/daemon/data:/opt/mcsmanager/daemon/data"
+            "${installPath}/daemon/logs:/opt/mcsmanager/daemon/logs"
+            "${installPath}/instance:/opt/mcsmanager/instance"
+
+            "/run/docker.sock:/var/run/docker.sock"
+          ];
+        };
       };
     };
+
+    environment.etc."timezone".text = "${config.time.timeZone}\n";
+
+    systemd.tmpfiles.rules = [
+      "d ${installPath} 0755 root root -"
+      "d ${installPath}/web 0755 root root -"
+      "d ${installPath}/web/data 0755 root root -"
+      "d ${installPath}/web/logs 0755 root root -"
+
+      "d ${installPath}/daemon 0755 root root -"
+      "d ${installPath}/daemon/data 0755 root root -"
+      "d ${installPath}/daemon/logs 0755 root root -"
+      "d ${installPath}/instance 0755 root root -"
+    ];
   };
-
-  environment.etc."timezone".text = "${config.time.timeZone}\n";
-
-  systemd.tmpfiles.rules = [
-    "d ${installPath} 0755 root root -"
-    "d ${installPath}/web 0755 root root -"
-    "d ${installPath}/web/data 0755 root root -"
-    "d ${installPath}/web/logs 0755 root root -"
-
-    "d ${installPath}/daemon 0755 root root -"
-    "d ${installPath}/daemon/data 0755 root root -"
-    "d ${installPath}/daemon/logs 0755 root root -"
-    "d ${installPath}/instance 0755 root root -"
-  ];
 }
