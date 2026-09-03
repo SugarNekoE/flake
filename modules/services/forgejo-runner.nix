4259e862be0cc1f2d51191153222fe0d056615c6
Asai Neko <sugar@sne.moe>
2026-09-03 22:18:40 +0800

    refactor(forgejo-runner): use forgejo recommend runner layout
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/services/forgejo-runner.nix
+++ modules/services/forgejo-runner.nix
@@ -1,13 +1,23 @@
 { inputs, ... }:
 let
-  withConfig = { sopsFile, forgejoUrl }: {
+  withConfig = sopsFile: {
     _class = "aspects";
     imports = [ inputs.self.modules.aspects.forgejo-runner ];
     nixosModule = { config, ... }: {
+      sops.secrets.forgejo-runner-url = {
+        inherit sopsFile;
+        format = "yaml";
+        key = "url";
+      };
+      sops.secrets.forgejo-runner-uuid = {
+        inherit sopsFile;
+        format = "yaml";
+        key = "uuid";
+      };
       sops.secrets.forgejo-runner-token = {
         inherit sopsFile;
-        format = "dotenv";
-        key = "";
+        format = "yaml";
+        key = "token";
       };
     };
   };
@@ -19,23 +29,57 @@ in
     {
       config,
       pkgs,
-      networking,
+      lib,
       ...
     }:
     {
-      services.gitea-actions-runner = {
-        package = pkgs.forgejo-runner;
-        instances.default = {
-          enable = true;
-          name = networking.hostName;
-          url = withConfig.forgejoUrl;
-          tokenFile = config.sops.secrets.forgejo-runner-token.path;
-          labels = [
-            "nixos-latest:docker://nixos/nix:latest"
-            "node-24:docker://node:24"
-            "go:docker://golang:latest"
-          ];
+      systemd.services.forgejo-runner = {
+        description = "Ephemeral Forgejo Actions Runner";
+
+        wantedBy = [ "multi-user.target" ];
+
+        after = [
+          "network-online.target"
+          "podman.service"
+        ];
+
+        wants = [ "network-online.target" ];
+
+        serviceConfig = {
+          Type = "simple";
+          Restart = "on-failure";
+          RestartSec = "5s";
+          WorkingDirectory = "/var/lib/forgejo-runner";
+          StateDirectory = "forgejo-runner";
         };
+
+        path = [
+          pkgs.coreutils
+          pkgs.podman
+        ];
+
+        preStart = ''
+          cd /var/lib/forgejo-runner
+          rm -f .runner
+          ${lib.getExe pkgs.forgejo-runner} register \
+            --no-interactive \
+            --url "$(cat ${config.sops.secrets.forgejo-runner-url.path})" \
+            --uuid "$(cat ${config.sops.secrets.forgejo-runner-uuid.path})" \
+            --token-url "file://${config.sops.secrets.forgejo-runner-token.path}" \
+            --labels ${
+              lib.escapeShellArg (
+                lib.concatStringsSep "," [
+                  "nixos-latest:docker://nixos/nix:latest"
+                  "node-24:docker://node:24"
+                  "go:docker://golang:latest"
+                ]
+              )
+            }
+        '';
+
+        script = ''
+          exec ${lib.getExe pkgs.forgejo-runner} one-job
+        '';
       };
     };
 }
