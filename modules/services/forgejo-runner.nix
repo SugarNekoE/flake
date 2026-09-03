6fd6d2506da387c01adc5796cba8ffa412065d63
Asai Neko <sugar@sne.moe>
2026-09-03 22:41:32 +0800

    refactor(forgejo-runner): use one-job run
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/services/forgejo-runner.nix
+++ modules/services/forgejo-runner.nix
@@ -51,36 +51,19 @@ in
           Type = "simple";
           Restart = "on-failure";
           RestartSec = "5s";
-          WorkingDirectory = "/var/lib/forgejo-runner";
+          WorkingDirectory = "/opt/forgejo-runner";
           StateDirectory = "forgejo-runner";
         };
 
-        path = [
-          pkgs.coreutils
-          pkgs.podman
-        ];
-
-        preStart = ''
-          cd /var/lib/forgejo-runner
-          rm -f .runner
-          ${lib.getExe pkgs.forgejo-runner} register \
-            --no-interactive \
+        script = ''
+          exec ${lib.getExe pkgs.forgejo-runner} one-job \
             --url "$(cat ${config.sops.secrets.forgejo-runner-url.path})" \
             --uuid "$(cat ${config.sops.secrets.forgejo-runner-uuid.path})" \
             --token-url "file://${config.sops.secrets.forgejo-runner-token.path}" \
-            --labels ${
-              lib.escapeShellArg (
-                lib.concatStringsSep "," [
-                  "nixos-latest:docker://nixos/nix:latest"
-                  "node-24:docker://node:24"
-                  "go:docker://golang:latest"
-                ]
-              )
-            }
-        '';
-
-        script = ''
-          exec ${lib.getExe pkgs.forgejo-runner}
+            --label ${lib.escapeShellArg} "nixos-latest:docker://nixos/nix:latest" \
+            --label ${lib.escapeShellArg} "node-24:docker://node:24" \
+            --label ${lib.escapeShellArg} "go:docker://golang:latest" \
+            --wait
         '';
       };
     };
