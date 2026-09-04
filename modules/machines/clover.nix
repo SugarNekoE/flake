876d7a52e69d4e14cc9508f288f2218be24d302d
Asai Neko <sugar@sne.moe>
2026-09-04 13:54:44 +0800

    feat(machines): add clover machine
    Signed-off-by: Asai Neko <sugar@sne.moe>

---
+++ modules/machines/clover.nix
@@ -0,0 +1,110 @@
+{ inputs, ... }:
+{
+  system = "x86_64-linux";
+  imports = with inputs.self.aspects; [
+    # system
+    efi
+    i18n
+    nfs
+
+    # roles
+    base
+    server
+
+    # services
+    aliyun
+    podman
+    forgejo
+  ];
+  diskoConfig = inputs.self.diskoConfigurations.xfs-with-quota;
+  nixos =
+    {
+      identity,
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      services.openssh.ports = [ 2222 ];
+      users.users.root.openssh.authorizedKeys.keys = [ identity.sshKeys.clover ];
+
+      networking.firewall.enable = lib.mkForce false;
+
+      services = {
+        cloud-init = {
+          enable = true;
+          network.enable = true;
+          settings = {
+            datasource_list = [ "AliYun" ];
+            preserve_hostname = false;
+            growpart = {
+              mode = "auto";
+              devices = [ "/" ];
+            };
+            resize_rootfs = true;
+          };
+        };
+        qemuGuest.enable = true;
+      };
+
+      system.autoUpgrade = {
+        enable = true;
+        upgrade = true;
+        operation = "switch";
+        flake = "git+https://forge.asnk.io/sugar/flake";
+        dates = "4:00";
+        flags = [
+          "--refresh"
+        ];
+      };
+
+      boot.kernel.sysctl = lib.mkForce {
+        "vm.swappiness" = "0";
+        "kernel.sysrq" = "1";
+        "net.ipv4.neigh.default.gc_stale_time" = "120";
+        "net.ipv4.conf.all.rp_filter" = "0";
+        "net.ipv4.conf.default.rp_filter" = "0";
+        "net.ipv4.conf.default.arp_announce" = "2";
+        "net.ipv4.conf.lo.arp_announce" = "2";
+        "net.ipv4.conf.all.arp_announce" = "2";
+        "net.ipv4.tcp_max_tw_buckets" = "5000";
+        "net.ipv4.tcp_syncookies" = "1";
+        "net.ipv4.tcp_max_syn_backlog" = "1024";
+        "net.ipv4.tcp_synack_retries" = "2";
+        "net.ipv4.tcp_slow_start_after_idle" = "0";
+      };
+    };
+
+  hardware =
+    {
+      config,
+      lib,
+      modulesPath,
+      ...
+    }:
+    {
+      imports = [
+        (modulesPath + "/profiles/qemu-guest.nix")
+      ];
+
+      boot.initrd.availableKernelModules = [
+        "nvme"
+      ];
+
+      disko.devices.disk.system.device = "/dev/vda";
+
+      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+      mounts.nfs = {
+        "/mnt/data" = {
+          remote = "121wm2ey8pigiyqr6qw-xqr37.cn-hongkong.nas.aliyuncs.com:/";
+          version = "v4";
+          minorVersion = "0";
+          extraOptions = [
+            "rsize=1048576"
+            "wsize=1048576"
+          ];
+        };
+      };
+    };
+}
