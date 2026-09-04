8ef149b2ce9d7a0ccd6a0586025e89ccb3632548
Asai Neko <sugar@sne.moe>
2026-09-04 16:35:42 +0800

    fix(mosdns): add to nixos aspect module
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/services/mosdns.nix
+++ modules/services/mosdns.nix
@@ -1,43 +1,44 @@
-{ pkgs, ... }:
 let
   installPath = "/opt/mosdns";
 in
 {
-  systemd.tmpfiles.rules = [
-    "d ${installPath} 0750 root root -"
-  ];
+  nixos = { pkgs, ... }: {
+    systemd.tmpfiles.rules = [
+      "d ${installPath} 0750 root root -"
+    ];
 
-  systemd.services.mosdns = {
-    description = "MosDNS DNS Forwarder";
-    wantedBy = [
-      "multi-user.target"
-    ];
-    after = [
-      "network-online.target"
-    ];
-    wants = [
-      "network-online.target"
-    ];
-    serviceConfig = {
-      ExecStart = ''
-        ${pkgs.mosdns}/bin/mosdns start \
-          -c ${installPath}/config.yaml
-      '';
-      Restart = "on-failure";
-      RestartSec = 5;
-      User = "mosdns";
-      Group = "mosdns";
-      StateDirectory = "mosdns";
-      ProtectSystem = "strict";
-      ProtectHome = true;
-      PrivateTmp = true;
+    systemd.services.mosdns = {
+      description = "MosDNS DNS Forwarder";
+      wantedBy = [
+        "multi-user.target"
+      ];
+      after = [
+        "network-online.target"
+      ];
+      wants = [
+        "network-online.target"
+      ];
+      serviceConfig = {
+        ExecStart = ''
+          ${pkgs.mosdns}/bin/mosdns start \
+            -c ${installPath}/config.yaml
+        '';
+        Restart = "on-failure";
+        RestartSec = 5;
+        User = "mosdns";
+        Group = "mosdns";
+        StateDirectory = "mosdns";
+        ProtectSystem = "strict";
+        ProtectHome = true;
+        PrivateTmp = true;
+      };
     };
-  };
 
-  users.users.mosdns = {
-    isSystemUser = true;
-    group = "mosdns";
-  };
+    users.users.mosdns = {
+      isSystemUser = true;
+      group = "mosdns";
+    };
 
-  users.groups.mosdns = { };
+    users.groups.mosdns = { };
+  };
 }
