cb9bbb3b002adc992557d597a2ca0597bfa9016a
Asai Neko <sugar@sne.moe>
2026-09-04 16:49:53 +0800

    fix(netbird-server): fix treafik tmpfiles permission
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/services/netbird-server.nix
+++ modules/services/netbird-server.nix
@@ -142,7 +142,7 @@ in
       "d /opt/netbird/crowdsec 0750 root root -"
       "d /opt/netbird/data 0750 root root -"
       "d /opt/netbird/data/netbird_data 0750 root root -"
-      "d /opt/netbird/data/netbird_traefik_letsencrypt 0750 root root -"
+      "d /opt/netbird/data/netbird_traefik_letsencrypt 0700 traefik treafik -"
       "d /opt/netbird/data/netbird_proxy_certs 0750 root root -"
       "d /opt/netbird/data/crowdsec_db 0750 root root -"
     ];
