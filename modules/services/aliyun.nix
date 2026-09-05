66d2b303ded1d111ebd3f63da6fbadeeda9225d5
Asai Neko <sugar@sne.moe>
2026-09-06 01:15:24 +0800

    feat(services/aliyun): add cloudmonitor support
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/services/aliyun.nix
+++ modules/services/aliyun.nix
@@ -21,32 +21,53 @@
 
         buildPhase = ''
           runHook preBuild
-
           export HOME=$TMPDIR
           export GOCACHE=$TMPDIR/go-cache
-
           go build \
             -mod=vendor \
             -trimpath \
             -o aliyun-service \
             .
-
           runHook postBuild
         '';
 
         installPhase = ''
           runHook preInstall
-
           mkdir -p $out/bin
           install -Dm755 aliyun-service $out/bin/aliyun-service
-
           runHook postInstall
+        '';
+      };
+      cloudmonitor-agent = pkgs.stdenv.mkDerivation {
+        pname = "aliyun-cloudmonitor-agent";
+        version = "latest";
+
+        src = ../../assets/cloudmonitor-agent.tar.gz;
+
+        installPhase = ''
+          mkdir -p $out/lib/cloudmonitor
+          mkdir -p $out/bin
+          cp -r * $out/lib/cloudmonitor/
+          chmod +x $out/lib/cloudmonitor/bin/* || true
+          chmod +x $out/lib/cloudmonitor/*.sh || true
+          ln -s $out/lib/cloudmonitor/bin/argusagent \
+            $out/bin/argusagent
+          ln -s $out/lib/cloudmonitor/cloudmonitorCtl.sh \
+            $out/bin/cloudmonitorctl
+        '';
+
+        postPatch = ''
+          substituteInPlace cloudmonitorCtl.sh \
+            --replace-fail \
+            "/usr/local/cloudmonitor" \
+            "\$out/lib/cloudmonitor"
         '';
       };
     in
     {
       environment.systemPackages = [
         aliyun-assist
+        cloudmonitor-agent
       ];
 
       systemd.tmpfiles.rules = [
@@ -55,23 +76,54 @@
 
       systemd.services.aliyun = {
         description = "Aliyun Assist";
-
         wantedBy = [ "multi-user.target" ];
         after = [
           "network-online.target"
           "cloud-final.service"
         ];
         wants = [ "network-online.target" ];
-
         serviceConfig = {
           Type = "simple";
-
           ExecStart = "${aliyun-assist}/bin/aliyun-service";
-
           Restart = "always";
           RestartSec = 5;
-
           User = "root";
+        };
+      };
+
+      systemd.services.cloudmonitor-agent = {
+        description = "Alibaba CloudMonitor Agent";
+        wantedBy = [
+          "multi-user.target"
+        ];
+        after = [
+          "network-online.target"
+        ];
+        wants = [
+          "network-online.target"
+        ];
+        serviceConfig = {
+          Type = "forking";
+          User = "root";
+          StateDirectory = "cloudmonitor";
+          LogsDirectory = "cloudmonitor";
+          WorkingDirectory = "/var/lib/cloudmonitor";
+          ExecStartPre = pkgs.writeShellScript "cloudmonitor-init" ''
+            if [ ! -e /var/lib/cloudmonitor/bin ]; then
+              ln -s \
+                ${cloudmonitor-agent}/lib/cloudmonitor/bin \
+                /var/lib/cloudmonitor/bin
+            fi
+            if [ ! -d /var/lib/cloudmonitor/conf ]; then
+                cp -r \
+                  ${cloudmonitor-agent}/lib/cloudmonitor/conf \
+                  /var/lib/cloudmonitor/
+              fi
+          '';
+          ExecStart = "${cloudmonitor-agent}/bin/cloudmonitorctl start";
+          ExecStop = "${cloudmonitor-agent}/bin/cloudmonitorctl stop";
+          Restart = "always";
+          RestartSec = 10;
         };
       };
     };
