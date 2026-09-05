20f7f21653841dcbe314c98ac7506e50f9b0f7b7
Asai Neko <sugar@sne.moe>
2026-09-05 12:36:11 +0800

    feat(plymouth): add nixos-mac-style
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/services/plymouth.nix
+++ modules/services/plymouth.nix
@@ -1,16 +1,29 @@
 _: {
-  nixos = {
+  nixos = { pkgs, ... }: {
     boot = {
       plymouth = {
         enable = true;
-        theme = "bgrt";
+        theme = "nixos-mac-style";
+        themePackages = [
+          (pkgs.stdenv.mkDerivation {
+            name = "plymouth-theme-nixos-mac-style";
+            version = "1.0";
+            src = ../../plymouth/nixos-mac-style;
+            installPhase = ''
+              mkdir -p $out/share/plymouth/themes/nixos-mac-style
+              cp -r . $out/share/plymouth/themes/nixos-mac-style/
+            '';
+          })
+        ];
       };
       consoleLogLevel = 3;
       initrd.verbose = false;
       kernelParams = [
         "quiet"
+        "splash"
         "udev.log_level=3"
         "rd.udev.log_level=3"
+        "rd.systemd.show_status=false"
       ];
     };
   };
