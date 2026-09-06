159b585c0ef71fc0a2d93ea0c0f0a63067c50083
Asai Neko <sugar@sne.moe>
2026-09-06 16:40:53 +0800

    perf(nfs): set nconnect option for nfs to 8
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/system/mounts/nfs.nix
+++ modules/system/mounts/nfs.nix
@@ -57,7 +57,7 @@
             "timeo=600"
             "retrans=2"
             "noresvport"
-            "nconnect=4"
+            "nconnect=8"
           ]
           ++ mount.extraOptions;
         }) mounts;
