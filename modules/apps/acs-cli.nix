808398e1540c1baf3503ad0e9eda047ba7b9db9f
Asai Neko <sugar@sne.moe>
2026-09-06 13:15:16 +0800

    refactor(acs-cli): replace the package name to acs-cli
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/apps/acs-cli.nix
+++ modules/apps/acs-cli.nix
@@ -2,11 +2,6 @@
 {
   flake-file.inputs.acs-k8s.url = "git+https://forge.asnk.io/sugar/acs-k8s";
 
-  systems = [
-    "x86_64-linux"
-    "aarch64-linux"
-  ];
-
   nixos = {
     nix.settings = {
       extra-substituters = [ "https://ack-nix.cachix.org" ];
@@ -18,9 +13,12 @@
 
   home =
     { pkgs, ... }:
+    let
+      acs-cli = inputs.acs-k8s.packages.${pkgs.stdenv.hostPlatform.system}.acs-cli;
+    in
     {
       home.packages = [
-        inputs.acs-k8s.packages.${pkgs.stdenv.hostPlatform.system}.acs
+        acs-cli
         pkgs.buildah
         pkgs.skopeo
       ];
