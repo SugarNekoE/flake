b4eab2bb3955c9ee6e4b1147fbafa1171d37d1a6
Asai Neko <sugar@sne.moe>
2026-09-11 13:07:38 +0800

    fix(davinci): temp hash mismatch override
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/apps/davinci.nix
+++ modules/apps/davinci.nix
@@ -1,8 +1,25 @@
 _: {
-  home = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      ffmpeg
-      unstable.davinci-resolve-studio
-    ];
-  };
+  home =
+    { pkgs, lib, ... }:
+    let
+      davinciResolveStudio = pkgs.unstable.davinci-resolve-studio.override {
+        runCommandLocal =
+          name: attrs:
+          pkgs.unstable.runCommandLocal name (
+            attrs
+            //
+              lib.optionalAttrs
+                (attrs.outputHash or null == "sha256-D5RjUukwKMpULrDfMJOPsPWW9FxhQ/IUMh76u5JLytA=")
+                {
+                  outputHash = "sha256-P+zu8/OuFcDcIkwV3UMq0qg9U2JEGRkKDP+VLQesZjw=";
+                }
+          );
+      };
+    in
+    {
+      home.packages = [
+        pkgs.ffmpeg
+        davinciResolveStudio
+      ];
+    };
 }
