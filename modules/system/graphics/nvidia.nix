7efce87ba5dd7547cd690ed58013c02c38a24478
Asai Neko <sugar@sne.moe>
2026-09-14 18:31:53 +0800

    feat(nvidia): patch nvidia for newer kernel-dev
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/system/graphics/nvidia.nix
+++ modules/system/graphics/nvidia.nix
@@ -1,6 +1,25 @@
 {
   nixos =
-    { config, pkgs, ... }:
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      kernel = config.boot.kernelPackages.kernel;
+      driver = (pkgs.unstable.linuxPackagesFor kernel).nvidiaPackages.latest;
+      openDriver = driver.open.overrideAttrs (old: {
+        postPatch = (old.postPatch or "") + ''
+          if grep -q 'struct dmem_cgroup_init {' \
+            ${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/include/linux/cgroup_dmem.h; then
+            substituteInPlace kernel-open/nvidia/os-interface.c \
+              --replace-fail 'dmem_cgroup_register_region(size, name)' \
+                'dmem_cgroup_register_region(&(struct dmem_cgroup_init){ .size = size }, "%s", name)'
+          fi
+        '';
+      });
+    in
     {
       hardware = {
         graphics = {
@@ -14,7 +33,11 @@
           modesetting.enable = true;
           powerManagement.enable = true;
           nvidiaSettings = true;
-          package = (pkgs.unstable.linuxPackagesFor config.boot.kernelPackages.kernel).nvidiaPackages.latest;
+          package =
+            driver
+            // lib.optionalAttrs (driver.version == "610.57.04") {
+              open = openDriver;
+            };
         };
       };
       services.xserver.videoDrivers = [ "nvidia" ];
