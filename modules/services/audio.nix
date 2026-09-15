6071be7ca2b275895e60c3cba2f7c605cdf7e507
Asai Neko <sugar@sne.moe>
2026-09-15 12:09:23 +0800

    feat(audio): config for hifi audio in pipewire and mpv
    Signed-off-by: Asai Neko <sugar@sne.moe>

--- modules/services/audio.nix
+++ modules/services/audio.nix
@@ -6,6 +6,22 @@ _: {
       alsa.enable = true;
       pulse.enable = true;
       wireplumber.enable = true;
+      extraConfig.pipewire."99-hifi" = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.allowed-rates" = [
+            44100
+            48000
+            88200
+            96000
+            176400
+            192000
+            352800
+            384000
+            768000
+          ];
+        };
+      };
     };
     services.pulseaudio.enable = false;
   };
